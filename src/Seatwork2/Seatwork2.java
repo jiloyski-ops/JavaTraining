91f6b4cc46bfe4374169a3f5059d531970e8b9e2
Michael Angelo M. Benolirao <mambenolirao@headoffice.corp.bpi.com.ph>
2025-12-04 18:33:07 +0800

    M2_Activity2

---
+++ src/Seatwork2/Seatwork2.java
@@ -0,0 +1,9 @@
+package Seatwork2;
+
+public class Seatwork2 {
+	public static void main(String [] args) {
+		Whiteboard wb = new Whiteboard ();
+		int total = wb.addNumbers(5,3);
+		System.out.println("The total is: "+ String.valueOf(total));
+	}
+}
